81f8540a25d621a6f0931ec45989af89e4ce23d8
JuanJo Arenas <dragonex579@gmail.com>
2025-09-03 08:47:54 -0500

    Creacion de BD y configuracion de Handler
    se hace models, seeders, factories y migrations, ademas de configurar el Handler

--- stockclem-api/app/Exceptions/Handler.php
+++ stockclem-api/app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\RouteNotFoundException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -18,6 +23,19 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
+    private $url = [
+        'article',
+        'category',
+        'entry',
+        'issue',
+        'person',
+        'presentation',
+        'role',
+        'supplier',
+        'unit',
+        'users'
+    ];
+
     /**
      * Register the exception handling callbacks for the application.
      */
@@ -26,5 +44,38 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+
+        $this->renderable(function(NotFoundHttpException $e, $request) {
+            //vamos a añadir de tipo api/ a la lista de urls
+            $urlFinal = preg_filter('/^/', 'api/', $this->url);
+            //vamos a añadir el sufijo / a la lista de urls
+            $urlFinal = preg_filter('/$/', '/*', $urlFinal);
+            //vamos ahora a comprobar si la url actual coincide con alguna de las urls de la lista
+            if($request->is($urlFinal)) 
+            {
+                return response()->json(['message' => 'Recurso no encontrado'], Response::HTTP_NOT_FOUND);
+            }
+        });
+
+        $this->renderable(function(MethodNotAllowedHttpException $e, $request){
+            return response()->json(['message' => 'Método no permitido'], Response::HTTP_METHOD_NOT_ALLOWED);
+        });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     */
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof AuthorizationException) 
+        {
+            return response()->json(['message' => 'No tienes permisos para acceder a este recurso'], Response::HTTP_FORBIDDEN);
+        }
+
+        if ($exception instanceof RouteNotFoundException) 
+            {
+            return response()->json(['message' => 'Debes iniciar sesion'], Response::HTTP_UNAUTHORIZED);
+        }
+        return parent::render($request, $exception);
     }
 }
